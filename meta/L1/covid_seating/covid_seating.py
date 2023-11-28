83fb2fa9f757c29390026c6d0d2ec3dc1d162175
Fyodor Wolf <FedyaNyet@users.noreply.github.com>
2023-11-28 16:32:49 +0000

    remove flask

--- meta/L1/covid_seating/covid_seating.py
+++ meta/L1/covid_seating/covid_seating.py
@@ -7,12 +7,12 @@ def getMaxAdditionalDinersCount(N: int, K: int, M: int, S: List[int]) -> int:
     ## This function Mutates the list S, by sorting it.
 
     S.sort()
-    print(f"--N:{N}:--K:{K}--S:{S}--")
+    # print(f"--N:{N}:--K:{K}--S:{S}--")
 
     def seats_available_between(start, end, buffer = K):
         spaces_between =  (end - start) - 1
         ret = max([0, math.floor( (spaces_between - K) / (K+1) )])
-        print(f"[{start}, {end}], space:{spaces_between}, result:{ret}")
+        # print(f"[{start}, {end}], space:{spaces_between}, result:{ret}")
         return ret
 
     if not len(S):
@@ -26,13 +26,6 @@ def getMaxAdditionalDinersCount(N: int, K: int, M: int, S: List[int]) -> int:
             
     available_seats += seats_available_between(S[-1], N+K+1)
 
-    print(f"total: {available_seats}")
+    # print(f"total: {available_seats}")
     return available_seats
 
-assert getMaxAdditionalDinersCount(10, 1, 2, [2,6]) ==  3 ## 0*0^0*0^0^
-assert getMaxAdditionalDinersCount(3, 1, 2, [0,2]) ==  0
-assert getMaxAdditionalDinersCount(6, 2, 2, [0,5]) ==  0
-assert getMaxAdditionalDinersCount(3, 1, 0, []) ==  2 # -^*^-
-assert getMaxAdditionalDinersCount(6, 1, 0, []) ==  3 # -^0^0^0-
-assert getMaxAdditionalDinersCount(15, 2, 3, [11, 6, 14]) ==  1 # ^--^--*----*--*
-assert getMaxAdditionalDinersCount(16, 2, 3, [6, 9, 15]) ==  2 # 00^00*00*00^00*0
---
+++ meta/L1/covid_seating/test_covid_seating.py
@@ -0,0 +1,16 @@
+import unittest
+from nose_parameterized import parameterized
+from .covid_seating import getMaxAdditionalDinersCount
+
+class MyTestCase(unittest.TestCase):
+    @parameterized.expand([
+        ('10-1-2', (10, 1, 2, [2,6]), 3),
+        ('3-1-2', (3, 1, 2, [0,2]), 0),
+        ('6-2-2' ,(6, 2, 2, [0,5]), 0),
+        ('3-1-0' ,(3, 1, 0, []), 2),
+        ('6-1-0' ,(6, 1, 0, []), 3),
+        ('15-2-3' ,(15, 2, 3, [11, 6, 14]), 1),
+        ('16-2-3' ,(16, 2, 3, [6, 9, 15]), 2),
+    ])
+    def test_invalid_input(self, _, args, expected):
+        self.assertEqual(getMaxAdditionalDinersCount(*args),  expected)
