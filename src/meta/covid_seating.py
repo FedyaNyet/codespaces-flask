ea0d876bde4ad48a659c0133047c9e2ddf8e07e7
Fyodor Wolf <FedyaNyet@users.noreply.github.com>
2023-11-29 14:11:44 +0000

    code cleanup

--- src/meta/covid_seating.py
+++ src/meta/covid_seating.py
@@ -5,27 +5,25 @@ import math
 def getMaxAdditionalDinersCount(N: int, K: int, M: int, S: List[int]) -> int:
     # Write your code here
     ## This function Mutates the list S, by sorting it.
+    assert(M == len(S))
 
     S.sort()
-    # print(f"--N:{N}:--K:{K}--S:{S}--")
-
-    def seats_available_between(start, end, buffer = K):
-        spaces_between =  (end - start) - 1
-        ret = max([0, math.floor( (spaces_between - K) / (K+1) )])
-        # print(f"[{start}, {end}], space:{spaces_between}, result:{ret}")
-        return ret
 
     if not len(S):
-        return seats_available_between(-K, N+K+1)
+        return seats_available_between(-K, N+K+1, K)
 
     available_seats = 0
-    available_seats += seats_available_between(-K, S[0])
+    available_seats += seats_available_between(-K, S[0], K)
     for idx, _ in enumerate(S):
         if idx < len(S)-1:
-            available_seats += seats_available_between(S[idx], S[idx+1])
+            available_seats += seats_available_between(S[idx], S[idx+1], K)
             
-    available_seats += seats_available_between(S[-1], N+K+1)
+    available_seats += seats_available_between(S[-1], N+K+1, K)
 
-    # print(f"total: {available_seats}")
     return available_seats
 
+
+def seats_available_between(start, end, buffer):
+    spaces_between =  (end - start) - 1
+    ret = max([0, math.floor( (spaces_between - buffer) / (buffer +1 ) )])
+    return ret
--- src/meta/test_meta.py
+++ src/meta/test_meta.py
@@ -1,6 +1,6 @@
 import unittest
 from parameterized import parameterized
-from meta.covid_seating import getMaxAdditionalDinersCount
+from meta.covid_seating import getMaxAdditionalDinersCount, seats_available_between
 
 class CovidSeatingTestCase(unittest.TestCase):
     @parameterized.expand([
@@ -12,5 +12,14 @@ class CovidSeatingTestCase(unittest.TestCase):
         ('15-2-3' ,(15, 2, 3, [11, 6, 14]), 1),
         ('16-2-3' ,(16, 2, 3, [6, 9, 15]), 2),
     ])
-    def test_invalid_input(self, _, args, expected):
+    def test_getMaxAdditionalDinersCount(self, _, args, expected):
         self.assertEqual(getMaxAdditionalDinersCount(*args),  expected)
+
+    @parameterized.expand([
+        ('0_4_1', (0,4,1), 1),
+        ('N3_1_1', (-3,1,1), 1),
+        ('N3_3_2', (-3,3,2), 1),
+        ('2_11_2', (2,11,2), 2)
+    ])
+    def test_seats_available_between(self, _, args, expected):
+        self.assertEqual(seats_available_between(*args), expected)
